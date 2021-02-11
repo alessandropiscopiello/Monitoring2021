512b91c6e372f9c453f98db99aa7d252071bf390
alessandropiscopiello <73694036+alessandropiscopiello@users.noreply.github.com>
2021-02-11 13:09:34 +0100

    Update R_code_Point_pattern_analysis.r

--- R_code_Point_pattern_analysis.r
+++ R_code_Point_pattern_analysis.r
@@ -100,3 +100,129 @@ Errore: unexpected '>' in ">"
 > dev.off()
 null device 
           1 
+###### interpolate case data
+
+marks(covid_planar) <- cases
+cases_map <- Smooth(covid_planar)
+
+plot(cases_map, col = cl)
+points(covid_planar)
+plot(coastlines, add = T)
+
+##### 
+# install sf package by:
+# install.packages("sf")
+library(sf)
+
+Spoints <- st_as_sf(covid, coords = c("lon", "lat"))
+
+cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
+plot(cases_map, col = cl)
+plot(Spoints, cex=Spoints$cases/10000, col = 'purple3', lwd = 3, add=T)
+
+library(rgdal)
+# put a smoother to the coastlines by resampling
+coastlines <- readOGR("ne_10m_coastline.shp")
+plot(coastlines, add=T)
+
+####### Leonardo Zabotti data
+
+# setwd for Windows
+# setwd("C:/lab/")
+
+# setwd Mac
+# setwd("/Users/yourname/lab/")
+
+# Linux
+setwd("~/lab/")
+
+leo <- read.table("dati_zabotti.csv", header=T, sep=",")
+
+head(leo)
+
+attach(leo)
+
+library(spatstat)
+summary(leo)
+
+leo_ppp <- ppp(x, y, c(2300000,2325000), c(5005000,5045000))
+
+plot(leo_ppp)
+
+density_map <- density(leo_ppp)
+plot(density_map)
+points(leo_ppp)
+
+# save the workspace:
+
+# windows and Mac: File | save workspace 
+
+# for linux
+# q()
+# click yes
+
+############ Interpolation of students' data
+
+# setwd("path/lab")
+
+# setwd for Windows
+# setwd("C:/lab/")
+
+# setwd Mac
+# setwd("/Users/yourname/lab/")
+
+# Linux
+setwd("~/lab/")
+
+load("point_pattern_analysis.RData")
+
+ls()
+head(leo)
+
+library(spatstat)
+attach(leo)
+marks(leo_ppp) <- chlh
+chlh_map <- Smooth(leo_ppp)
+
+cl <- colorRampPalette(c('yellow','orange','red','green'))(100)  
+plot(chlh_map, col=cl)
+points(leo_ppp)
+
+# Exercise: do the same for chls in the sediment
+marks(leo_ppp) <- chls
+chls_map <- Smooth(leo_ppp)
+
+plot(chls_map, col=cl)
+points(leo_ppp)
+
+# multipanel
+par(mfrow=c(1,3))
+
+# first graph: density map
+plot(density_map, col=cl)
+points(leo_ppp)
+
+# second graph
+plot(chlh_map, col=cl)
+points(leo_ppp)
+
+# third graph
+plot(chls_map, col=cl)
+points(leo_ppp)
+
+# Exercise: build a multipanel with 3 rows and 1 column
+# multipanel
+par(mfrow=c(3,1))
+
+# first graph: density map
+plot(density_map, col=cl)
+points(leo_ppp)
+
+# second graph
+plot(chlh_map, col=cl)
+points(leo_ppp)
+
+# third graph
+plot(chls_map, col=cl)
+points(leo_ppp)
+
